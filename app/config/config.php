4999fc80476b34d3475ffe836b4a4203e186b201
Douglas Sales <douglassales18@hotmail.com>
2021-11-05 14:23:42 -0300

    Adicionado funções globais de encriptografar e descriptografar strings - encrypt() e decrypt()

--- app/config/config.php
+++ app/config/config.php
@@ -3,4 +3,5 @@
 define('BASE_URL', 'http://localhost:8000');
 define('DEFAULT_LANGUAGE', 'default');
 define('DEFAULT_FORMAT_DATE', 'Y-m-d H:i');
+define('ENCRYPT_KEY', 'qkwjdiw239&&jdafweihbrhnan&^%$ggdnawhd4njshjwuuO');
 define('URI_COUNT', 0);
--- system/helpers/functions.php
+++ system/helpers/functions.php
@@ -58,6 +58,19 @@ function callFile(String $file) {
     }
 }
 
+function encrypt(String $string) {
+    $encryption_key = base64_decode(ENCRYPT_KEY);
+    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+    $encrypted = openssl_encrypt($string, 'aes-256-cbc', $encryption_key, 0, $iv);
+    return base64_encode($encrypted . '::' . $iv);
+}
+
+function decrypt(String $string) {
+    $encryption_key = base64_decode(ENCRYPT_KEY);
+    list($encrypted_data, $iv) = array_pad(explode('::', base64_decode($string), 2),2,null);
+    return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
+}
+
 function getVersion() {
     echo VERSION;
 }
