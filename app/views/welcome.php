b9d12319f738464c2fbca29ac2ef2d20eb43130b
Douglas Sales <douglassales18@hotmail.com>
2021-11-05 14:35:51 -0300

    Ajustando exemplo de uso da tradução

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -2,7 +2,7 @@
 <!-- To include other files, just use the following code: -->
 <?php echo callFile('header'); ?>
 
-<?php $this->lang('nova_palavra') ?> <br><br>
+<?php $this->lang('example') ?> <br><br>
 
 <div class="main text-center">
     Welcome to <b>Mudrock Framework <?php getVersion() ?></b>
